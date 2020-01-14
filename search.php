8756b6dde038817b85a82d8d2f2b5cfee501785a
Giannis Katsaros <katsarosgiannis1@gmail.com>
2020-01-14 20:10:50 +0200

    fixed search

--- search.php
+++ search.php
@@ -37,25 +37,39 @@
 				   mysqli_set_charset($link, "utf8");
 					$q = $_POST['q'];
 					
-					$sql = "SELECT * FROM line WHERE code LIKE '$q%'";
-					$result = mysqli_query($link, $sql);
-					if ($result != "") 
+					$sql = "SELECT * FROM line WHERE code = '$q'";
+					$result = $link->query($sql);
+					$row = $result->fetch_array(MYSQLI_ASSOC);
+
+					if ($row['code'] != "") 
 					{
-						while($row = mysqli_fetch_assoc($result))
-						{
-							echo "Τύπος: ".$row['transportation_type'];
-							echo "<br>";
-							echo "Στάσεις: ".$row['number_of_stops'];
-							echo "<br>";
-							echo "Αφετηρία: ".$row['starting_point_stop_id'];
-							echo "<br>";
-							echo "Τέρμα: ".$row['destination_stop_id'];
-							echo "<br>";
-							if($row['is_circular'] == 1)
-								echo "Κυκλική διαδρομή: ΝΑΙ";
-							else
-								echo "Κυκλική διαδρομή: ΟΧΙ";
-						}
+						echo "Κωδικός: ".$row['code'];
+						echo "<br>";
+						echo "Τύπος: ".$row['transportation_type'];
+						echo "<br>";
+						echo "Στάσεις: ".$row['number_of_stops'];
+						echo "<br>";
+						if($row['is_circular'] == 1)
+							echo "Κυκλική διαδρομή: ΝΑΙ";
+						else
+							echo "Κυκλική διαδρομή: ΟΧΙ";
+						echo "<br>";
+
+						$idStart = $row['starting_point_stop_id'];
+						$idStop = $row['destination_stop_id'];
+
+						$sql2 = "SELECT name FROM stop WHERE id = '$idStart'";
+						$result2 = $link->query($sql2);
+						$name2 =  $result2->fetch_array(MYSQLI_ASSOC);
+						echo "Αφετηρία: ".$name2['name'];
+						echo "<br>";
+						
+						$sql3 = "SELECT name FROM stop WHERE id = '$idStop'";
+						$result3 = $link->query($sql3);
+						$name3 = $result3->fetch_array(MYSQLI_ASSOC);
+					
+						echo "Τέρμα: ".$name3['name'];
+						echo "<br>";
 					}
 					else
 					{
